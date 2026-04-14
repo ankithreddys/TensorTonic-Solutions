2a8f2b2d66de7ce29981e5142bfb3d8f8c219b3e
ANKITH REDDY SUBHANPURAM <93903287+ankithreddys@users.noreply.github.com>
2026-04-13 21:01:15 -0400

    Update solution for image-histogram

--- image-histogram/image-histogram.py
+++ image-histogram/image-histogram.py
@@ -5,9 +5,5 @@ def image_histogram(image):
     """
     # Write code here
     image = np.asarray(image)
-    bins = np.zeros((256,))
-    for i in range(image.shape[0]):
-        for j in range(image.shape[1]):
-            pix_val = image[i][j]
-            bins[pix_val] += 1
-    return bins.tolist()
+    hist = np.bincount(image.flatten(), minlength=256)
+    return hist.tolist()
